d0866744b29af66cee198ed3b98be40f606dce48
akp01211 <akp012@ucsd.edu>
2026-05-19 23:34:15 -0700

    Update vmalloc.c

--- vmalloc.c
+++ vmalloc.c
@@ -2,7 +2,6 @@
 #include "vmlib.h"
 
 
-
 /**
  * Allocate a memory block with the given minimum size on the simulated heap.
  * If allocation succeeds, return a pointer to the start of the new block's
@@ -11,10 +10,10 @@
  * Read the section for vmalloc in the PA writeup for how to implement this
  * function.
  */
-static struct b_header *block_n(struct b_header *b){
-    return (struct b_header *)((char *)b+BLKSZ(b));
+static struct block_header *block_n(struct block_header *b){
+    return (struct block_header *)((char *)b+BLKSZ(b));
 }
-static void footer_w(struct b_header *b){
+static void footer_w(struct block_header *b){
     struct block_footer *f =(struct block_footer *)((char *)b + BLKSZ(b) -sizeof(struct block_footer));
     f->size = BLKSZ(b);
 }
@@ -26,11 +25,11 @@ static void footer_w(struct b_header *b){
         return NULL;
  
     // Rounding up multiple of 16
-    size_t nec =ROUND_UP(size +sizeof(struct b_header),BLKALIGN);
+    size_t nec =ROUND_UP(size +sizeof(struct block_header),BLKALIGN);
  
     // Best fit alg
-    struct b_header *best = NULL;
-    for(struct b_header *b = heapstart; b->size_status !=VM_ENDMARK; b =block_n(b)){
+    struct block_header *best = NULL;
+    for(struct block_header *b = heapstart; b->size_status !=VM_ENDMARK; b =block_n(b)){
 
 
 
@@ -47,7 +46,7 @@ static void footer_w(struct b_header *b){
                 best =b;
             }
     
- 
+    }
 
 
     if(best == NULL)
@@ -63,11 +62,11 @@ static void footer_w(struct b_header *b){
 
         best->size_status=nec | (best->size_status & VM_PREVBUSY) |VM_BUSY;
  
-        struct b_header *split= block_n(best);
+        struct block_header *split= block_n(best);
         split->size_status =rem |VM_PREVBUSY;
         footer_w(split);
  
-        struct b_header *next =block_n(split);
+        struct block_header *next =block_n(split);
 
 
         if(next->size_status!= VM_ENDMARK){
@@ -81,7 +80,7 @@ static void footer_w(struct b_header *b){
 
 
 
-        struct b_header *nxt= block_n(best);
+        struct block_header *nxt= block_n(best);
         if(nxt->size_status !=VM_ENDMARK){
 
 
@@ -92,6 +91,5 @@ static void footer_w(struct b_header *b){
         }
     }
  
-    return (void *)((char *)best+sizeof(struct b_header));
+    return (void *)((char *)best+sizeof(struct block_header));
 }
- }
